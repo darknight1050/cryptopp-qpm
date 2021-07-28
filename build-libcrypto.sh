80f6d6faa242d38df51932eebae47d0a538cf5e3
darknight1050 <darkdeveloping1050@gmail.com>
2021-07-28 22:08:50 +0200

    Fix build script

--- build-libcrypto.sh
+++ build-libcrypto.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-
-cd ./cryptopp/TestScripts
-
+(
 #Change this env variable to the number of processors you have
 if [ -f /proc/cpuinfo ]; then
 	JOBS=$(grep flags /proc/cpuinfo |wc -l)
@@ -12,20 +10,23 @@ else
 	JOBS=2
 fi
 
-export TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/linux-x86_64
-export ANDROID_NDK_ROOT=$NDK
-export ANDROID_SDK_ROOT=/usr/lib/android-sdk
-export PATH=$TOOLCHAIN/bin:$PATH
+export ANDROID_NDK_ROOT=/c/Development/Tools/android-ndk-r21e
+export ANDROID_SDK_ROOT=/
+export PATH=$PATH:$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/windows-x86_64/bin
+echo $PATH
 export ANDROID_API=28
 export ANDROID_ABI=23
 export ANDROID_CPU=arm64-v8a
-source setenv-android.sh
-cd ..
-make -j$JOBS -f GNUmakefile-cross distclean
-make -j$JOBS -f GNUmakefile-cross static dynamic
 
+source setenv-android.sh
+
+cd ./cryptopp
+make -j$JOBS -f GNUmakefile-cross distclean
+make -j$JOBS -f GNUmakefile-cross static
 cd ..
 
 mkdir ./shared
 find ./cryptopp -name \*.h -exec cp {} ./shared \;
 cp ./cryptopp/libcryptopp.a ./
+
+)
